6e7de40798a5ffa057a92126702e9a0579190a87
fc <corral.fdo@gmail.com>
2024-04-13 18:11:41 -0700

    Finalizando proyecto, actualizado localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useReducer, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { cartReducer, initialState } from "./reducers/cart-reducer"
@@ -6,6 +6,9 @@ import { cartReducer, initialState } from "./reducers/cart-reducer"
 function App() {
 
   const [state, dispatch] = useReducer(cartReducer, initialState)
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
 
   return (
     <>
@@ -25,10 +28,9 @@ function App() {
               dispatch={dispatch}
             />
           ))}
-
         </div>
       </main>
-      
+
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
